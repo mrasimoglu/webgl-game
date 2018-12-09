2d9fb1f8821bc1d88ba36f4c62e5d164555cca44
Mustafa RASIMOGLU <mrasimoglu@hotmail.com>
2018-12-09 08:15:39 +0300

    0912

--- armature.js
+++ armature.js
@@ -1,5 +1,5 @@
 var VERTEX_SHADER = [
-    '#define MaxBone 200',
+    '#define MaxBone 100',
     ' #ifdef GL_ES',
     'precision highp float;',
 
@@ -8,7 +8,9 @@ var VERTEX_SHADER = [
     'attribute vec3 a_Normal;',
     'attribute vec3 a_BoneIds;',
     'attribute vec3 a_Weights;',
+    'attribute vec2 a_TexCoords;',
 
+    'uniform bool u_HasBones;',
     'uniform mat4 u_ModelMatrix;',
     'uniform mat4 u_ViewMatrix;',
     'uniform mat4 u_ProjectionMatrix;',
@@ -17,18 +19,22 @@ var VERTEX_SHADER = [
     
     'varying vec3 v_Normal;',
     'varying vec3 v_FragPos;',
+    'varying vec2 v_TexCoords;',
     
     'void main(){',
-   // 'vec3 ttt = vec3(a_Position.x, -a_Position.z, a_Position.y);',
+        'vec4 newVertex = vec4(a_Position, 1.0);',
+        'vec3 tmp = a_Normal;',
+        'if(u_HasBones){',
         'mat4 boneMatrix = u_Bones[int(a_BoneIds[0])] * a_Weights[0];',
         'boneMatrix += (u_Bones[int(a_BoneIds[1])]  * a_Weights[1]);',
         'boneMatrix += (u_Bones[int(a_BoneIds[2])]  * a_Weights[2]);',
-        'vec4 newVertex = boneMatrix * vec4(a_Position, 1.0);',
+        'newVertex = boneMatrix * newVertex;',
+        'tmp = mat3(boneMatrix) * a_Normal;',
+        '}',
         'gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_ModelMatrix * newVertex;',
         'v_FragPos = (u_ModelMatrix * newVertex).xyz;',
-        'vec3 tmp = mat3(boneMatrix) * a_Normal;',
         'v_Normal =  mat3(u_NormalMatrix) * tmp;',
- 
+        'v_TexCoords = a_TexCoords;',
         'gl_PointSize = 10.0;',
     '}',
 ].join("\n");
@@ -39,29 +45,50 @@ var FRAGMENT_SHADER = [
     'precision highp float;',
     '  #endif',
     
-    'uniform vec3 u_objectColor;',
+    'uniform sampler2D u_t_diffuse;',
+    'uniform vec3 u_specular;',
+    'uniform float u_shiness;',
+    'uniform bool u_HasTex;',
+    'uniform bool u_HasSpecular;',
+    'uniform vec3 u_ambient;',
+    'uniform vec3 u_diffuse;',
+
+    
     'uniform vec3 u_lightColor;',
     'uniform vec3 u_lightPos;',
     'uniform vec3 u_viewPos;',
     'varying vec3 v_Normal;',
     'varying vec3 v_FragPos;',
+    'varying vec2 v_TexCoords;',
     'void main(){',
         'vec3 norm = normalize(v_Normal);',
         //Ambient
-        'float ambientStrenght = 0.1;',
-        'vec3 ambient = ambientStrenght * u_lightColor;',
+        'float ambientStrenght = 0.9;',
+        'vec3 ambient = vec3(1,1,1);',
+        'vec3 diffuse = vec3(1,1,1);',
         //Diffuse
         'vec3 lightDirection = normalize(u_lightPos-v_FragPos);',
         'float diff = max(dot(lightDirection, norm), 0.0);',
-        'vec3 diffuse = u_lightColor * diff;',
+        
+
+        'if(!u_HasTex){',
+            'ambient = ambientStrenght * u_lightColor * u_ambient;',
+            'diffuse = u_lightColor * diff * u_diffuse;',
+        '}else{',
+            'diffuse = u_lightColor * diff * vec3(texture2D(u_t_diffuse, v_TexCoords));',
+            'ambient = ambientStrenght * u_lightColor * vec3(texture2D(u_t_diffuse, v_TexCoords));',
+        '}',
         //Specular
         'float specularStrength = 0.5;',
         'vec3 viewDir = normalize(u_viewPos - v_FragPos);',
         'vec3 reflectDir = reflect(-lightDirection, norm); ',
-        'float spec = pow(max(dot(viewDir, reflectDir), 0.0),64.0);',
-        'vec3 specular = specularStrength * spec * u_lightColor; ',
+        'float spec = pow(max(dot(viewDir, reflectDir), 0.0), u_shiness);',
+        'vec3 specular =  u_lightColor * (spec * u_specular); ',
+        'if(!u_HasSpecular){',
+            'specular = vec3(0,0,0);',
+        '}',
         //Mix
-        'gl_FragColor = vec4(u_objectColor * (specular + ambient + diffuse ), 1);',
+        'gl_FragColor = vec4(diffuse + ambient + specular, 1);',
     '}',
 ].join("\n");
 
@@ -71,10 +98,31 @@ var ay = 20;
 
 var kx=-6,ky=3,kz=-3;//light position
 
-function main(){
+
+
+var Init = function() 
+{
+    	
+			var modelTexts = [ "models/wolf.json"];
+
+			var promises = [];
+			modelTexts.forEach(function(element)
+			{
+				promises.push(loadJSONResource(element));
+			});
+			Promise.all(promises).then((res) => {
+                modelObjects = res;
+                console.log(res);
+                Run(modelObjects);
+			});
+		
+}
+
+function Run(jsonmodels){
     var canvas = document.getElementById("mycanvas");
     var gl = getWebGLContext(canvas);
-
+    var canvashud = document.getElementById("hud");
+    var hud = canvashud.getContext("2d");
     if(!gl)
     {
         console.log("Gl Error");
@@ -86,17 +134,24 @@ function main(){
         console.log("Shader Error");
         return;
     }
-
-    loadJSONResource("models/animatedHuman.json").then((mod) => {
-        console.log(mod);
-        var model = mod;
-        model = new Model(model);
-        
-        var shad = new Shader(gl, ['u_ModelMatrix', 'u_ViewMatrix', 'u_ProjectionMatrix', 'u_NormalMatrix', 'u_Bones', 'u_objectColor', 'u_objectColor', 'u_lightColor', 'u_lightPos', 'u_viewPos']);    
         gl.enable(gl.DEPTH_TEST);
+        var shad = new Shader(gl, ['a_TexCoords', 'a_Normal','a_Position', 'a_BoneIds', 'a_Weights'], 
+        ['u_HasSpecular', 'u_HasTex', 'u_ambient', 'u_diffuse','u_shiness', 'u_specular', 'u_HasBones','u_ModelMatrix', 
+            'u_ViewMatrix', 'u_ProjectionMatrix', 'u_NormalMatrix', 'u_Bones', 'u_lightColor', 'u_lightPos', 'u_viewPos']);    
         
-        var Lx=0,Ly=0,Lz=-50; //Camera position
-        model.animator.playAnimation(0);
+        var models = [];
+        jsonmodels.forEach((model) => {
+            models.push(new Model(model, shad));
+           
+        });
+        models.forEach((model)=>{
+            model.animator.playAnimation(9);
+        });
+        
+        var Lx=0,Ly=0,Lz=-10; //Camera position
+        var before = Date.now();
+        var now = Date.now();
+        
         const loop = () => {
             gl.clearColor(0.4, 0.4, 0.4, 1.0);
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -109,32 +164,41 @@ function main(){
             gl.uniformMatrix4fv(shad.u_ViewMatrix, false, viewMatrix.elements);
             
             var projectionMatrix = new Matrix4();
-            projectionMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
+            projectionMatrix.setPerspective(30, canvas.width/canvas.height, 1, 10000);
             gl.uniformMatrix4fv(shad.u_ProjectionMatrix, false, projectionMatrix.elements);
             
-            
+            //
+            models.forEach((model)=>{
+                
             model.callAnimator();
-      
-            var modelMatrix = new Matrix4();
-            modelMatrix.setIdentity();
-            model.drawModel(model.root, shad, modelMatrix);
-            
-            var joint = model.getJointByName(model.root, "SolKol");
+                var modelMatrix = new Matrix4();
+                modelMatrix.setIdentity();
+                //modelMatrix.scale(0.1,0.1,0.1);
+                model.drawModel(model.root, shad, modelMatrix);
+            });
+           
+            //var joint = model.getJointByName(model.root, "SolKol");
            // var joint2 = model.getJointByName(model.root, "Cone_002");
           //  joint.rotateJoint(270, 1, 0, 1);
             //joint2.rotateJoint(60, 1, 0, 0);
           
-            //return;
-            /*  //draw light
-            modelMatrix.setIdentity();
+            //draw light
+       /*     modelMatrix.setIdentity();
             gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
             initArrayBuffer(new Float32Array([kx, ky, kz]), 'a_Position', 3, 0, 0);
-            gl.drawArrays(gl.POINTS,0 ,1);*/
-
-           
+            gl.drawArrays(gl.POINTS,0 ,1);
+*/
+            now = Date.now();
+            var DeltaTime = now-before;
+            before = now;
+            var fps = parseInt(1000/DeltaTime);
+            hud.clearRect(0,0,400,400);
+            hud.font = "30px Arial";
+            hud.fillText(fps, 50 ,30);
+        
             requestAnimationFrame(loop);
         }  
-        requestAnimationFrame(loop);
+    loop();
 
 
         document.onkeydown = (event) => {
@@ -155,12 +219,12 @@ function main(){
             }
         };
 
-        canvas.onmousedown = (event) => {;
+        canvashud.onmousedown = (event) => {;
             var x = event.clientX, y = event.clientY;
             var difx = 0;
             var dify = 0;
             
-            canvas.onmousemove = (event2) => {
+            canvashud.onmousemove = (event2) => {
                 difx = event2.clientX - x;
                 dify = event2.clientY - y;
                 var rMatrix =  new Matrix4();
@@ -174,10 +238,10 @@ function main(){
                 y = event2.clientY;
             };
         };
-        canvas.onmouseup = (event) => {
-            canvas.onmousemove = null;
+        canvashud.onmouseup = (event) => {
+            canvashud.onmousemove = null;
         };
-        canvas.onmousewheel = (event) => {
+        canvashud.onmousewheel = (event) => {
             rate  = Lx*Lx + Ly*Ly + Lz*Lz;
             rate = Math.sqrt(rate)*event.deltaY/-100;
 
@@ -187,12 +251,6 @@ function main(){
             console.log(rate);
             //Lz += event.deltaY/100*-1;
         };
-    }
-
-    ).catch((err) => {
-        console.log("model not loaded error :" + err);
-    })
-
 }
 
 function is(){
